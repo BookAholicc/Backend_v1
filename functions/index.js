b819090ac666abbb21eae5c23f67d05967eadfd9
nanborklabs <nandhu12195@gmail.com>
2017-08-07 00:46:40 +0530

    Firebase Plugged

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,29 @@
-
-
 'use strict';
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+
+/* Example */
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
